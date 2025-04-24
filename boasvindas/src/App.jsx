f946d8471d4a40de1c5d33abf57e9b70decb6ac8
Miguel333-droid <miguelmarianooo07@gmail.com>
2025-04-24 11:05:18 -0300

    24-04-25

---
+++ boasvindas/src/App.jsx
@@ -0,0 +1,20 @@
+import BoasVindas from "./Exemplo1"
+
+function App() {
+  return (
+   <div align="center">
+    <h1>Boas Vindas Ao React com JavaScript</h1>
+
+    <p>Este é o arquivo App.jsx que será o nosso ponto de partida para
+       o desenvolvimento de aplicação React utilizando JavaScript
+    </p>
+
+    <BoasVindas nome="Miguel" />
+    
+
+    
+   </div>
+  )
+}
+
+export default App
---
+++ boasvindas/src/Exemplo1.jsx
@@ -0,0 +1,8 @@
+export default function BoasVindas({ nome }) {
+    return (
+        <div>
+            Olá {nome} seja bem vindo ao React.
+        </div>
+    );
+
+}
